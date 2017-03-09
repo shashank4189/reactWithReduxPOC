3b0ef5af7cc82abac5cdc057394136f6b0f663ff
Shashank <Shashank.barnawal@compunnel.com>
2017-03-09 12:07:22 +0530

    routing in react

--- main.js
+++ main.js
@@ -5,9 +5,16 @@
 import React from 'react';
 import rend1 from 'react-dom';
 import App from './App.jsx';
+import {Router, IndexRoute,Link, Route, browserHistory} from 'react-router';
 
 function tick() {
-  rend1.render(<App  />, document.getElementById('mainApp'));
+  //rend1.render(<App  />, document.getElementById('mainApp'));
+  rend1.render((
+    <Router history ={browserHistory}>
+      <Route path="/" component={App}>
+      <Route></Route>
+      </Route>
+    ), document.getElementById('mainApp'));
 }
 
 setInterval(tick,1000);
