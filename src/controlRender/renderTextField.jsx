85e62872ae083c3579c5c957b9d46f1ff43c064a
Shashank <Shashank.barnawal@compunnel.com>
2017-03-25 11:37:21 +0530

    porp implemntation in rendertextfield

--- src/controlRender/renderTextField.jsx
+++ src/controlRender/renderTextField.jsx
@@ -3,20 +3,20 @@ import React ,{ PropTypes } from 'react';
 import TextField from 'material-ui/TextField';
 
 
-/*export const renderTextField = props => (
+export const renderTextField = props => (
   <TextField hintText={props.label}
              floatingLabelText={props.label}
-             errorText={props.touched && props.error}
+             errorText={props.meta.touched && props.meta.error}
              {...props.input}
              {...props.custom}
   />
-)*/
+)
 
-export const renderTextField = ({input, label, meta: {touched, error}, ...custom}) => (
+/*export const renderTextField = ({input, label, meta: {touched, error}, ...custom}) => (
   <TextField hintText={label} floatingLabelText={label} errorText={touched && error}
              {...input}
              {...custom}
-  />)
+  />)*/
 
 
 
