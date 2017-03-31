5d514186a3535b549bc7db1bbfa7c5fc477b2da4
Shashank <Shashank.barnawal@compunnel.com>
2017-04-01 01:34:45 +0530

    data change

--- src/components/leftDrawer.jsx
+++ src/components/leftDrawer.jsx
@@ -55,7 +55,7 @@ class LeftDrawer extends Component {
 
       <Drawer
         docked={true}
-        open={true}>{/* this.props.navDrawerOpen*/}
+        open={this.props.navDrawerOpen}>
         <div style={styles.logo}>
           Issue Tracker
         </div>
